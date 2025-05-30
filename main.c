b4127e1c9ed45aa35be38668f305c5bafef6424c
Rogelio Hdz <hdc.rogelio@gmail.com>
2025-05-30 17:59:53 -0600

    feat: mpich

--- main.c
+++ main.c
@@ -14,9 +14,15 @@ static int KERNEL_SIZE = 55;
 
 int main(int argc, char *argv[]) {
     int rank, size;
+    char hostname[MPI_MAX_PROCESSOR_NAME];
+    int hostname_len;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MPI_Get_processor_name(hostname, &hostname_len);
+    printf("[RANK %d] Ejecutando en host: %s\n", rank, hostname);
 
     if (argc < 3 || argc > 600) {
         if (rank == 0)
